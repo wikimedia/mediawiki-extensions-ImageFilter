d7efe78fbf1cab6e636d9ba0c814841e29e35437
Tim Starling <tstarling@wikimedia.org>
2017-06-23 20:10:33 +1000

    Initial import from RationalWiki source
    License: GPL, per https://www.mediawiki.org/wiki/Extension:ImageFilter

---
+++ ImageFilter.i18n.php
@@ -0,0 +1,7 @@
+<?php
+$messages = array();
+
+$messages['en'] = array(
+  'tog-displayfiltered'            => 'Display filtered images',
+  'nsfw-warning'                   => '<sup>Warning: NSFW Image!</sup>',
+);
