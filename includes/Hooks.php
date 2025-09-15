2db648e44d24b703b7a9c4e63426432c28025af3
Reedy <reedy@wikimedia.org>
2025-09-15 23:17:26 +0100

    Setup basic composer testing
    Some more modernisation too
    
    Remove PagePropLinkInvalidations from extension.json
    
    Change-Id: I5dd63f0c984d1b796e7072312f045f80ef8648d0

--- includes/Hooks.php
+++ includes/Hooks.php
@@ -2,31 +2,38 @@
 
 namespace MediaWiki\Extension\ImageFilter;
 
+use HtmlArmor;
+use MediaWiki\FileRepo\File\File;
 use MediaWiki\Hook\GetDoubleUnderscoreIDsHook;
 use MediaWiki\Hook\ImageBeforeProduceHTMLHook;
 use MediaWiki\Hook\PageRenderingHashHook;
 use MediaWiki\Linker\LinkRenderer;
 use MediaWiki\Page\PageProps;
+use MediaWiki\Parser\Parser;
 use MediaWiki\Preferences\Hook\GetPreferencesHook;
+use MediaWiki\Title\Title;
 use MediaWiki\User\UserOptionsManager;
+use User;
+
 // HtmlArmor isn't namespaced yet in MW 1.43, it seems...
 // use Wikimedia\HtmlArmor\HtmlArmor;
 
-class Hooks implements PageRenderingHashHook,
-	ImageBeforeProduceHTMLHook, GetPreferencesHook, GetDoubleUnderscoreIDsHook {
-	private UserOptionsManager $userOptionsManager;
-	private PageProps $pageProps;
-	private LinkRenderer $linkRenderer;
+class Hooks implements
+	PageRenderingHashHook,
+	ImageBeforeProduceHTMLHook,
+	GetPreferencesHook,
+	GetDoubleUnderscoreIDsHook
+{
+	public function __construct(
+		private readonly UserOptionsManager $userOptionsManager,
+		private readonly PageProps $pageProps,
+		private readonly LinkRenderer $linkRenderer,
+	) {
+	}
 
-	/**
-	 * @param \MediaWiki\User\UserOptionsManager $userOptionsManager
-	 * @param \MediaWiki\Page\PageProps $pageProps
-	 * @param \MediaWiki\Linker\LinkRenderer $linkRenderer
-	 */
-	public function __construct( $userOptionsManager, $pageProps, $linkRenderer ) {
-		$this->userOptionsManager = $userOptionsManager;
-		$this->pageProps = $pageProps;
-		$this->linkRenderer = $linkRenderer;
+	public static function setup(): void {
+		global $wgPagePropLinkInvalidations;
+		$wgPagePropLinkInvalidations['imagefilter-nsfw'] = 'imagelinks';
 	}
 
 	/**
@@ -34,8 +41,8 @@ class Hooks implements PageRenderingHashHook,
 	 * preference. This is needed to avoid exposing users who've not opted into NSFW images
 	 * to them.
 	 *
-	 * @param string $hash Parser cache key hash
-	 * @param \User $user
+	 * @param string &$hash Parser cache key hash
+	 * @param User $user
 	 * @param array &$forOptions
 	 */
 	public function onPageRenderingHash( &$hash, $user, &$forOptions ) {
@@ -48,13 +55,14 @@ class Hooks implements PageRenderingHashHook,
 	 * Alter the generated image HTML depending on the value of the user's "displayfiltered" preference.
 	 *
 	 * @param null $unused
-	 * @param \MediaWiki\Title\Title &$title
-	 * @param \MediaWiki\FileRepo\File\File &$file
+	 * @param Title &$title
+	 * @param File &$file
 	 * @param array &$frameParams Contains the 'thumbnail', 'align' and 'caption' parameters for thumbnailed images
-	 * @param array &$handlerParams Contains values like 'width' (in pixels, without the unit) and 'targetlang' (ISO-639 code)
+	 * @param array &$handlerParams Contains values like 'width' (in pixels, without the unit) and 'targetlang'
+	 *  (ISO-639 code)
 	 * @param int &$time
 	 * @param string &$res
-	 * @param \MediaWiki\Parser\Parser $parser
+	 * @param Parser $parser
 	 * @param string &$query
 	 * @param int &$widthOption
 	 * @return bool True if we should do no custom processing, false when the user has NOT
@@ -76,7 +84,7 @@ class Hooks implements PageRenderingHashHook,
 		$linker = $this->linkRenderer;
 		if ( isset( $frameParams['caption'] ) && $frameParams['caption'] !== '' ) {
 			// HtmlArmor to render <i> ... </i> etc.
-			$res = $linker->makeLink( $title, new \HtmlArmor( $frameParams['caption'] ) );
+			$res = $linker->makeLink( $title, new HtmlArmor( $frameParams['caption'] ) );
 		} else {
 			$res = $linker->makeLink( $title );
 		}
@@ -88,7 +96,7 @@ class Hooks implements PageRenderingHashHook,
 	/**
 	 * Register the new user preference to have it show up on Special:Preferences.
 	 *
-	 * @param \User &$user
+	 * @param User $user
 	 * @param array &$preferences
 	 */
 	public function onGetPreferences( $user, &$preferences ) {
